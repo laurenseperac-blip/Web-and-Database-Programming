963cbb3a28662bd2d81da376b6a9937c4b248581
laurenseperac-blip <laurenseperac@gmail.com>
2026-05-11 21:41:00 -0400

    user routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,13 +2,64 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 
-router.get('/getAllUsers', async (req, res) => {
+router.post("/register", async (req, res) => {
+
     try {
-        const users = await User.getAllUsers();
-        res.send(users);
+
+        const result = await User.registerUser(req.body);
+
+        res.send(result);
+
     } catch (err) {
-        res.status(500).send({ message: err.message });
+
+        res.status(400).send({
+            message: err.message
+        });
     }
+});
+
+router.post("/login", async (req, res) => {
+
+    try {
+
+        const user = await User.loginUser(
+            req.body.email,
+            req.body.password
+        );
+
+        res.send(user);
+
+    } catch (err) {
+
+        res.status(401).send({
+            message: err.message
+        });
+    }
+});
+
+router.get("/", async (req, res) => {
+
+    const users = await User.getAllUsers();
+
+    res.send(users);
+});
+
+router.put("/:id", async (req, res) => {
+    const result = await User.updateUser(
+        req.params.id,
+        req.body.firstName,
+        req.body.lastName,
+        req.body.email
+    );
+
+    res.send(result);
+});
+
+router.delete("/:id", async (req, res) => {
+
+    const result = await User.deleteUser(req.params.id);
+
+    res.send(result);
 });
 
 module.exports = router;
