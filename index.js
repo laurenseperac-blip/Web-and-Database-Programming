1d551521f99ff995a33d8daf553e0658d51495d4
laurenseperac-blip <laurenseperac@gmail.com>
2026-05-11 21:36:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
+const path = require("path")
 
 app.use(express.json());
 
 const userRoutes = require("./server/routes/user");
 const assignmentRoutes = require("./server/routes/assignment");
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");  
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");  
+    next();
+})
+
+app.use(express.static(__dirname + "/public"))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/login.html')))
 
 app.use("/users", userRoutes);
 app.use("/assignments", assignmentRoutes);
