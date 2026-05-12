0a763fff319b7ab56d085c53bbc587e5dea5dc74
laurenseperac-blip <laurenseperac@gmail.com>
2026-05-11 21:40:51 -0400

    assignment routes

--- server/routes/assignment.js
+++ server/routes/assignment.js
@@ -2,13 +2,44 @@ const express = require("express");
 const router = express.Router();
 const Assignment = require("../models/assignment");
 
-router.get('/getAllAssignments', async (req, res) => {
-    try {
-        const assignments = await Assignment.getAllAssignments();
-        res.send(assignments);
-    } catch (err) {
-        res.status(500).send({ message: err.message });
-    }
+router.post("/", async (req, res) => {
+
+    const result = await Assignment.createAssignment(req.body);
+
+    res.send(result);
+});
+
+router.get("/:student_id", async (req, res) => {
+
+    const assignments = await Assignment.getAssignmentsByUser(
+        req.params.student_id
+    );
+
+    res.send(assignments);
+});
+
+router.get("/", async (req, res) => {
+
+    const assignments = await Assignment.getAllAssignments();
+
+    res.send(assignments);
+});
+
+router.put("/:id", async (req, res) => {
+
+    const result = await Assignment.updateAssignment(
+        req.params.id,
+        req.body.assignment_name
+    );
+
+    res.send(result);
+});
+
+router.delete("/:id", async (req, res) => {
+
+    const result = await Assignment.deleteAssignment(req.params.id);
+
+    res.send(result);
 });
 
 module.exports = router;
